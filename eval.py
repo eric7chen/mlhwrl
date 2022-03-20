b98dcbbe5286adcf6423af163e1312139eb29bb5
Nick <nicholas.j.thompson-1@ou.edu>
2022-03-20 15:20:54 -0500

    update tdlearn

--- eval.py
+++ eval.py
@@ -8,7 +8,6 @@ from game.tic_tac_toe import TicTacToe
 from rand_player import RandomPlayer
 from game.Player import Player
 from tdlearn import TDAgent
-from tdlearn import learn as tdlearn
 
 PLAYER1 = 1
 PLAYER2 = -1
@@ -33,7 +32,7 @@ def play_game(board: Board, player1: Player, player2: Player) -> int:
         
 
 
-def battle_q(player1: Player, player2: Player, num_games: int = 10000, silent: bool = False):
+def battle(player1: Player, player2: Player, num_games: int = 10000, silent: bool = False):
     board = Board()
     draw_count = 0
     wins_1 = 0
@@ -49,14 +48,14 @@ def battle_q(player1: Player, player2: Player, num_games: int = 10000, silent: b
     print(f"after {num_games} game we have draws: {draw_count}, player1 wins: {wins_1}, player2 wins: {wins_2}")
     return (wins_1, wins_2, draw_count)
 
-def eval_qplayers(p1 : Player, p2 : Player, games_per_battle = 100, num_battles = 100, silent: bool = False):
+def eval_players(p1 : Player, p2 : Player, games_per_battle = 100, num_battles = 100, silent: bool = False):
     p1_wins = []
     p2_wins = []
     draws = []
     count = []
 
     for i in range(num_battles):
-        p1win, p2win, draw = battle_q(p1, p2, games_per_battle, silent)
+        p1win, p2win, draw = battle(p1, p2, games_per_battle, silent)
         p1_wins.append(p1win*100.0/games_per_battle)
         p2_wins.append(p2win*100.0/games_per_battle)
         draws.append(draw*100.0/games_per_battle)
@@ -71,72 +70,14 @@ def eval_qplayers(p1 : Player, p2 : Player, games_per_battle = 100, num_battles 
     plt.xlabel('Training Iterations')
     plt.savefig("myplot3.png")
 
-def battle_td(player1, player2, num_games):
-    env = TicTacToe()
-    player1.set_env(env)
-    player2.set_env(env)
-    first = True
-    oneWins = 0
-    twoWins = 0
-    draws = 0
-    env.reset()
-    for i in range(num_games):
-        while True:
-            if first:
-                action, greedy = player1.action()
-                previous_state = env.board.hash()
-                reward, _, over = env.step(action)
-                current_state = env.board.hash()
-            else:
-                action, greedy = player2.action()
-                previous_state = env.board.hash()
-                reward, _, over = env.step(action)
-                current_state = env.board.hash()
-            first = not first
-            # print(env.board)
-            if over:
-                if (reward == 1 and first) or (reward == -1 and not first):
-                    oneWins += 1
-                elif (reward == -1 and first) or (reward == 1 and not first):
-                    twoWins += 1
-                else:
-                    draws += 1
-                # print(env.board)
-                env.reset()
-                break
-    return oneWins * 100.0 / num_games, twoWins * 100.0 / num_games, draws * 100.0 / num_games
-
-def eval_tdplayers(games_per_battle = 100, num_battles = 200):
-    ones = []
-    twos = []
-    draws = []
-    count = []
-    for i in range(0, 100, 5):
-        one, two = tdlearn(i)
-        one.reset_explore()
-        two.reset_explore()
-        a, b, c = battle_td(one, two, 1000)
-        ones.append(a)
-        twos.append(b)
-        draws.append(c)
-        count.append(i)
-
-    print(ones, twos, draws, count)
-    plt.plot(count, ones, color="red", label="Player One Win")
-    plt.plot(count, twos, color="green", label="Player Two Win")
-    plt.plot(count, draws, color="blue", label="Draw")
-    plt.xlabel("Training Iterations")
-    plt.ylabel("Outcome %")
-    plt.legend()
-    plt.savefig("myplottd.png")
-
 
 #train q agent   
-player1 = QAgent()
-player2 = QAgent()
-eval_qplayers(player1, player2, 40, 200)
+# player1 = QAgent()
+# player2 = QAgent()
+# eval_players(player1, player2, 40, 200)
 
 # train tdagent
-
-#eval_tdplayers(20, 100)
+player1 = TDAgent()
+player2 = TDAgent()
+eval_players(player1, player2, 40, 200)
 
--- tdlearn.py
+++ tdlearn.py
@@ -7,31 +7,47 @@ from game.tic_tac_toe import Board
 from game.tic_tac_toe import TicTacToe
 from game.Player import Player
 
+# rewards for win, draw, and loss
+RES_WIN = 1.0
+RES_DRAW = 0.5
+RES_LOSS = 0.0
+
 
 class TDAgent(Player):
-    def __init__(self, environment):
-        self.environment = environment
-        self.learning_rate = 0.1
-        self.exploration_rate = 0.1
-        self.values = numpy.zeros(3 ** (self.environment.board.size ** 2 + 1))
+    def __init__(self):
+        self.side = None
+        self.alpha = 0.1
+        self.gamma = 0.95
+        self.epsilon = 0.1
+        self.vtable = numpy.zeros(3 ** (3 ** 2 + 1))
+        # history for applying reward at the end of an episode
+        self.history = []
+    
+    def get_move(self, board: Board) -> int:
+        possible = board.possible_actions()
+        # take random move epsilon % of the time
+        if numpy.random.binomial(1, self.epsilon):
+            move = tuple(random.choice(possible))
+            return (3*move[0] + move[1])
+        copies = [copy.deepcopy(board) for i in possible]
+        for action, boardcopy in zip(possible, copies):
+            boardcopy.take_turn(tuple(action))
+        hashes = [boardcopy.hash() for boardcopy in copies]
+        # move to state with best value
+        move = possible[numpy.argmax(self.vtable[hashes])]
+        return (3*move[0] + move[1])
+    
+    def move(self, board: Board):
+        over, _ = board.is_over()
+        move = self.get_move(board)
+        action = (move // 3, move % 3)
+        assert (board.is_possible(action))
+        assert (not over)
 
-    def record(self, initial, resulting, reward, over):
-        if over:
-            self.values[resulting] = reward
-        # V(s) = V(s) + alpha * [V(s') - V(s)]
-        self.values[initial] = self.values[initial] + self.learning_rate * (
-            self.values[resulting] - self.values[initial]
-        )
-
-    def action(self):
-        possible = self.environment.board.possible_actions()
-        if numpy.random.binomial(1, self.exploration_rate):
-            return tuple(random.choice(possible)), False
-        copies = [copy.deepcopy(self.environment.board) for i in possible]
-        for action, board in zip(possible, copies):
-            board.take_turn(tuple(action))
-        hashes = [board.hash() for board in copies]
-        return tuple(possible[numpy.argmax(self.values[hashes])]), True
+        self.history.append(board.hash())
+        board.take_turn(action)
+        over, winner = board.is_over()
+        return (winner, board, over)
 
     def reset_explore(self):
         self.exploration_rate = 0
@@ -39,84 +55,29 @@ class TDAgent(Player):
     def set_env(self, environment):
         self.environment = environment
     
-    def move(self, env: Board):
-        possible = env.possible_actions()
-        copies = [copy.deepcopy(env) for i in possible]
-        for action, board in zip(possible, copies):
-            board.take_turn(tuple(action))
-        hashes = [board.hash() for board in copies]
-
-        env.take_turn(tuple(possible[numpy.argmax(self.values[hashes])]))
-        over, winner = env.is_over()
-        return (winner, env, over)
-    
     def final_result(self, result: int):
-        pass
+        if (result == -1 and self.side == -1) or (result == 1 and self.side == 1):
+            final_value = RES_WIN
+        elif (result == -1 and self.side == 1) or (result == 1 and self.side == -1):
+            final_value = RES_LOSS
+        elif (result == 0):
+            final_value = RES_DRAW
+        else:
+            raise ValueError("Unexpected game result")
+
+        self.history.reverse()
+        next_val = -1.0
+
+        for h in self.history:
+            # on first pass
+            if next_val < 0:
+                self.vtable[h] = final_value
+            else:
+                self.vtable[h] = self.vtable[h] + self.alpha * (final_value + self.gamma * next_state - self.vtable[h])
+            next_state = self.vtable[h]
+        
+
 
     def new_game(self, side: int):
-        pass
-
-
-def learn(iters):
-    env = TicTacToe()
-    playerOne = TDAgent(env)
-    playerTwo = TDAgent(env)
-    episodes = range(int(iters))
-    for i in episodes:
-        playerOne_turn = True
-        while True:
-            if playerOne_turn:
-                action, greedy = playerOne.action()
-            else:
-                action, greedy = playerTwo.action()
-            playerOne_turn = not playerOne_turn
-            prev_state = env.board.hash()
-            reward, _, over = env.step(action)
-            new_state = env.board.hash()
-            if greedy:
-                playerOne.record(prev_state, new_state, reward, over)
-                playerTwo.record(prev_state, new_state, -reward, over)
-            if over:
-                env.reset()
-                break
-    return playerOne, playerTwo
-
-
-def play(one, two, games):
-    env = TicTacToe()
-    playerOne = one
-    playerTwo = two
-    playerOne.set_env(env)
-    playerTwo.set_env(env)
-    iters = games
-    first = True
-    oneWins = 0
-    twoWins = 0
-    draws = 0
-    env.reset()
-    for i in range(iters):
-        while True:
-            if first:
-                action, greedy = playerOne.action()
-                previous_state = env.board.hash()
-                reward, _, over = env.step(action)
-                current_state = env.board.hash()
-            else:
-                action, greedy = playerTwo.action()
-                previous_state = env.board.hash()
-                reward, _, over = env.step(action)
-                current_state = env.board.hash()
-            first = not first
-            # print(env.board)
-            if over:
-                if (reward == 1 and first) or (reward == -1 and not first):
-                    oneWins += 1
-                elif (reward == -1 and first) or (reward == 1 and not first):
-                    twoWins += 1
-                else:
-                    draws += 1
-                # print(env.board)
-                env.reset()
-                break
-    return oneWins * 100.0 / games, twoWins * 100.0 / games, draws * 100.0 / games
-
+        self.side = side
+        self.history = []
